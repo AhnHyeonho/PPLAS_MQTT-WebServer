e6bd26bd31ae9854bc1cd12a1af844842433f141
ahndding <ahndding@gmail.com>
2019-09-25 15:56:14 +0900

    임시저장

--- PPLAS_MQTT-WebServer/src/setting/Setting.java
+++ PPLAS_MQTT-WebServer/src/setting/Setting.java
@@ -1,7 +1,7 @@
 package setting;
 
 public class Setting {
-	public static String dbURL = "jdbc:mysql://116.126.97.126:3306/pplas?characterEncoding=UTF-8&serverTimezone=UTC";
+	public static String dbURL = "jdbc:mysql://116.126.97.126:3306/pplas?autoReconnect=true&useSSL=false";
 	public static String dbID = "root";
 	public static String dbPassword = "1234";
 }
